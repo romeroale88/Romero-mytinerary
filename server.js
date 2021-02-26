eb687d4165d258d77e20d2822232499e121d0295
romeroale <romeroale.88@gmail.com>
2021-02-26 10:05:05 -0300

    heroku

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express')  
+const path = require('path')
 require('dotenv').config()
 const cors = require('cors')
 const router = require('./routes/index')
@@ -6,9 +7,22 @@ require('./config/database')
 
 const app = express()
 
-app.use(cors())
 app.use(express.json())
+app.use(cors())
 
 app.use('/api', router)
 
-app.listen(4000, () => console.log("app listening on port 4000"))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname+"/client/build/index.html"))
+    })
+  }
+  const port = process.env.PORT || 4000
+  const host = process.env.HOST || '0.0.0.0'
+  
+  //app.listen(port, host,  console.log('app listening'))
+  /*Server port e iniciacion*/
+  app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}`))
+
+// app.listen(4000, () => console.log("app listening on port 4000"))
